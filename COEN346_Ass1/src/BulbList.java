490ba1ee66515a7927df7e5501b47d8797093eed
abroyouknow <57846353+abroyouknow@users.noreply.github.com>
2020-02-05 16:06:53 -0500

    added comments

--- COEN346_Ass1/src/BulbList.java
+++ COEN346_Ass1/src/BulbList.java
@@ -1,18 +1,18 @@
 
 public class BulbList {
-	private int bulbCount=0;
-	private int[] bulbNum;
+	private int bulbCount=0; 		//holds the array index for the array of defective bulbs
+	private int[] bulbNum;			//int array that will be passed the positions of the burnt bulbs
 	
 	BulbList(int a){
-		bulbNum = new int[a];
+		bulbNum = new int[a];		//overloaded constructor creates an int array who's size is passed
 	}
 	
 	public void register(int pos) {
-		bulbNum[bulbCount++] = pos;
+		bulbNum[bulbCount++] = pos;	//any bulb identified as "burnt" will have its position noted (index is also incremented)
 	}
 	
 	public int[] getList() {
-		return bulbNum;
+		return bulbNum;			//return the array of burnt bulb positions to caller
 	}
 
 }
